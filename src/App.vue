12e189751ffd1807d78835f02b63b53d11bfd7ad
Adewole Adewumi <AAdewumi@cegep-heritage.qc.ca>
2024-11-29 09:52:08 -0500

    starting off with vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Garage from './components/Garage.vue';
+import mydata from './data/cardata.json'
+</script>
+
+<template>
+  <main>
+    <Garage :data="mydata" />
+  </main>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+  
+}
+</style>
---
+++ src/components/Garage.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref("Adewole's Garage")
+
+const props = defineProps({
+    data: Array
+})
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+
+        <div class="content">
+            <div v-for="car in props.data" :id="car.vin">
+                <p>{{ car.year }} {{ car.manufacturer }} {{ car.model }}</p>
+                <p v-if="car.safetied">Has been safetied</p>
+                <p v-else>Has not been safetied</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
+}
+</style>
